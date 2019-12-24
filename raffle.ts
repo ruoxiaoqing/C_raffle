21117279329e076fe9fcf8a6881647df01afef23
ff98sha <20861337+ff98sha@users.noreply.github.com>
2019-12-24 23:22:31 +0800

    add -403 status code

--- raffle.ts
+++ raffle.ts
@@ -98,7 +98,7 @@ class Raffle {
       }
       else {
         tools.Log(this._user.nickname, title, id, raffleAward.body)
-        if (raffleAward.body.code === 400 || raffleReward.body.code === -403) return 'raffleBan'
+        if (raffleAward.body.code === 400 || raffleAward.body.code === -403) return 'raffleBan'
       }
     }
   }
